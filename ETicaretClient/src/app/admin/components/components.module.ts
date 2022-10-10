d99bf5760f596189ba07b63a1160982cfd1202ce
saadetbozkan <saadetbozkan@gmail.com>
2022-10-10 06:41:45 +0300

    add admin and ui modules.

---
+++ ETicaretClient/src/app/admin/components/components.module.ts
@@ -0,0 +1,20 @@
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
+import { ProductsModule } from './products/products.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    CustomersModule,
+    OrdersModule,
+    DashboardModule,
+  ]
+})
+export class ComponentsModule { }
