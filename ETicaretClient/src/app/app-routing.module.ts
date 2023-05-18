cd63473e1f97f710c79d7ed62b01a2c748b6409d
saadetbozkan <saadetbozkan@gmail.com>
2023-05-18 09:09:19 +0300

    routerActiveLink was added

--- ETicaretClient/src/app/app-routing.module.ts
+++ ETicaretClient/src/app/app-routing.module.ts
@@ -6,19 +6,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/common/auth.guard';
 
 const routes: Routes = [
-  { path:"", component: HomeComponent},
-  {path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule)},
-  {path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule)},
-  {path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule)},
-  {path: "login", loadChildren: () => import("./ui/components/login/login.module").then(module => module.LoginModule)},
+  { path: "", component: HomeComponent },
+  { path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
+  { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
+  { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) },
+  { path: "login", loadChildren: () => import("./ui/components/login/login.module").then(module => module.LoginModule) },
   {
-    path: "admin", component: LayoutComponent, children:[
-      {path: "", component : DashboardComponent, canActivate:[AuthGuard]},
-      {path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule),canActivate:[AuthGuard]},
-      {path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule),canActivate:[AuthGuard]},
-      {path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule),canActivate:[AuthGuard]},
-    ],canActivate:[AuthGuard]
-  }
+    path: "admin", component: LayoutComponent, children: [
+      { path: "", component: DashboardComponent, canActivate: [AuthGuard] },
+      { path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule), canActivate: [AuthGuard] },
+      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule), canActivate: [AuthGuard] },
+      { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule), canActivate: [AuthGuard] },
+      { path: "**", component: DashboardComponent, canActivate: [AuthGuard] }
+    ], canActivate: [AuthGuard]
+  },
+  { path: "**", component: HomeComponent }
 ];
 
 @NgModule({
